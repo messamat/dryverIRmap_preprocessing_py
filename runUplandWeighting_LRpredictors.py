2c42c3d1581c4a8d1be8e5249ce5e145051abe2d
messamat <mathis.messager@mail.mcgill.ca>
2023-12-12 10:52:50 -0500

    Concatenate tables of LR predictors

--- runUplandWeighting_LRpredictors.py
+++ runUplandWeighting_LRpredictors.py
@@ -93,7 +93,7 @@ def flowacc_extract_nc(in_ncpath, in_var, in_template_extentlyr, in_template_res
                                                 new_field_name=id_field, new_field_alias=id_field)
 
                     field_to_rename = [f.name for f in arcpy.ListFields(out_table) if f.name not in
-                                        [id_field, 'X', 'Y', arcpy.Describe(out_table).OIDFieldName]]
+                                       [id_field, 'X', 'Y', arcpy.Describe(out_table).OIDFieldName]]
                     arcpy.management.AlterField(out_table,
                                                 field=field_to_rename[0],
                                                 new_field_name=f'{fieldroot}_{i + 1}',
@@ -203,26 +203,75 @@ if analysis_period == 'future':
 
     #Indices done: 0: 'gfdl-esm4_r1i1p1f1_w5e5_historical_wetdays', 1: 'gfdl-esm4_r1i1p1f1_w5e5_ssp126_wetdays',
     # 2:  'gfdl-esm4_r1i1p1f1_w5e5_ssp585_wetdays', 4: watergap2_2e_gfdl_esm4_w5e5_ssp126_2015soc_from_histsoc_qrdifoverql
-    for var in list(LRpred_vardict.keys())[4:8]: #Using the list(dict.keys()) allows to slice it the keys
+    for var in list(LRpred_vardict.keys())[8:10]: #Using the list(dict.keys()) allows to slice it the keys
         in_var_formatted = re.sub(r'[ -.]', '_', var)
 
         scratchgdb_var = os.path.join(LRpred_resdir_gcms, 'scratch_{}.gdb'.format(in_var_formatted))
         pathcheckcreate(scratchgdb_var)
 
-        # Subset pourpoints if needed
-        flowacc_extract_nc(in_ncpath=LRpred_vardict[var][0],
-                           in_var=in_var_formatted,
-                           in_template_extentlyr=flowdir_grid,
-                           in_template_resamplelyr=flowdir_grid,
-                           pxarea_grid=pxarea_grid,
-                           uparea_grid=uparea_grid,
-                           flowdir_grid=flowdir_grid,
-                           out_resdir=LRpred_resdir_gcms,
-                           scratchgdb=scratchgdb_var,
-                           integer_multiplier=LRpred_vardict[var][1],
-                           in_location_data=dryver_netpourpoints_sub,
-                           out_tabdir = LRpred_tabgdb,
-                           id_field='DRYVER_RIVID',
-                           fieldroot=re.split('_', in_var_formatted)[-1],
-                           save_raster=False,
-                           save_tab=True)
+        final_csvtab = os.path.join(LRpred_resdir_gcms, "{}.csv".format(in_var_formatted))
+
+        if not arcpy.Exists(final_csvtab):
+            # Subset pourpoints if needed
+            fieldroot = re.split('_', in_var_formatted)[-1]
+            flowacc_extract_nc(in_ncpath=LRpred_vardict[var][0],
+                               in_var=in_var_formatted,
+                               in_template_extentlyr=flowdir_grid,
+                               in_template_resamplelyr=flowdir_grid,
+                               pxarea_grid=pxarea_grid,
+                               uparea_grid=uparea_grid,
+                               flowdir_grid=flowdir_grid,
+                               out_resdir=LRpred_resdir_gcms,
+                               scratchgdb=scratchgdb_var,
+                               integer_multiplier=LRpred_vardict[var][1],
+                               in_location_data=dryver_netpourpoints_sub,
+                               out_tabdir = LRpred_tabgdb,
+                               id_field='DRYVER_RIVID',
+                               fieldroot= fieldroot,
+                               save_raster=False,
+                               save_tab=True)
+
+            pred_nc = xr.open_dataset(LRpred_vardict[var][0])
+            new_variable_name = re.sub('\\s', '_', list(pred_nc.variables)[-1])
+            out_tablist = [os.path.join(LRpred_tabgdb, f'{in_var_formatted}_{i + 1}') for i in
+                           range(pred_nc.coords.dims['Time (Month)'])]
+            def get_LRpred_pd(in_gdbtab, in_fieldroot, in_dtype, last_col=False):
+                print(in_gdbtab)
+
+                var_colname = "{0}_{1}".format(
+                    in_fieldroot,
+                    re.findall('[0-9]{1,3}$', os.path.split(in_gdbtab)[1])[0])
+
+                if last_col:
+                    step_pd = pd.DataFrame(data=arcpy.da.SearchCursor(in_gdbtab,
+                                                                      [f.name for f in arcpy.ListFields(in_gdbtab)][-1]),
+                                           dtype=in_dtype)
+                    step_pd.columns = [var_colname]
+                else:
+                    rcols = ['DRYVER_RIVID', var_colname]
+                    step_pd = pd.DataFrame(data=arcpy.da.SearchCursor(in_gdbtab, rcols),
+                                           columns=rcols,
+                                           dtype=in_dtype)
+                return(step_pd)
+
+            if all([arcpy.Exists(out_tab) for out_tab in out_tablist]):
+                print("Concatenating tables...")
+                out_tab = get_LRpred_pd(in_gdbtab=out_tablist[0],
+                                        in_fieldroot=fieldroot,
+                                        in_dtype=np.int32)
+
+                invar_types = {'qrdifoverql': np.int32,
+                               'wetdays': np.int16}
+
+                for in_tab in out_tablist[1:]:
+                    temp_tab = get_LRpred_pd(in_gdbtab=in_tab,
+                                             in_fieldroot=fieldroot,
+                                             in_dtype=invar_types[fieldroot],
+                                             last_col=True)
+                    if len(temp_tab) == len(out_tab):
+                        out_tab = pd.concat([out_tab, temp_tab], axis=1)
+                    else:
+                        raise Exception("gdb table is not the same length as main table")
+
+                print("Writing out {}...".format(final_csvtab))
+                out_tab.to_csv(final_csvtab)
